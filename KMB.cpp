c8b74de70498bc2962fbeb3e541dc1cce90e6a4e
Snehasish Kumar <ska124@sfu.ca>
2015-03-21 02:45:58 -0700

    Added input checks

--- KMB.cpp
+++ KMB.cpp
@@ -7,23 +7,39 @@
 using namespace std;
 
 typedef boost::adjacency_list <boost::listS, boost::vecS > BoostGraph;
+string InputFileName, OutputFileName;
+float multicastFraction = 0.0;
 
 int main(int argc, char* argv[])
 {
     if(argc != 4)
     {
-        cout << "Usage: " << argv[0] << " <multicast_set_string> <input.dot> <output.dot>\n";
-        cout << "multicast_set_string = 1,2,3\n";
+        cerr << "Usage: " << argv[0] << " <Multicast Fraction> <Input.dot> <Output.dot>\n";
         return 1; 
     }
-    string InputFileName, OutputFileName;
-    InputFileName.assign(argv[1]);
-    OutputFileName.assign(argv[2]);
+
+    try
+    {
+        multicastFraction = stof(string(argv[1]));
+        if(multicastFraction < 0 || multicastFraction > 1)
+            throw invalid_argument("Fraction should be > 0 and < 1.");
+    }
+    catch(exception &err)
+    {
+        cerr << err.what() << endl;
+        cerr << "Invalid fraction argument\n";
+        return 1;
+    }
+
+    InputFileName.assign(argv[2]);
+    OutputFileName.assign(argv[3]);
 
     ifstream InputFile(InputFileName.c_str(), ios::in);
     ofstream OutputFile(OutputFileName.c_str(), ios::out);
 
     assert(OutputFile.is_open() && InputFile.is_open() && "Error opening files");
+
+    BoostGraph Network(0);
 
     return 0;
 }
