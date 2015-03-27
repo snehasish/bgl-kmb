98507472af7ea900907c770d2603a15da2e1235e
Snehasish Kumar <ska124@sfu.ca>
2015-03-26 17:01:59 -0700

    Step3 - debugging

--- KMB.cpp
+++ KMB.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 #include <string>
 #include <fstream>
 #include <set>
@@ -27,10 +28,10 @@ struct edge_p
     unsigned penwidth;
 };
 
-typedef boost::adjacency_list <boost::listS, boost::vecS, boost::undirectedS, vertex_p, edge_p, boost::no_property > graph_t;
+typedef adjacency_list <listS, vecS, undirectedS, vertex_p, edge_p, no_property > graph_t;
 
-typedef boost::graph_traits<graph_t>::vertex_descriptor Vertex;
-typedef boost::graph_traits<graph_t>::edge_descriptor Edge;
+typedef graph_traits<graph_t>::vertex_descriptor Vertex;
+typedef graph_traits<graph_t>::edge_descriptor Edge;
 
 // Typedefs for intermediate graphs
 
@@ -46,15 +47,16 @@ struct edge_q
     double weight;
 };
 
-typedef boost::adjacency_list <boost::listS, boost::vecS, boost::undirectedS, vertex_q, edge_q, boost::no_property > graph_q_t;
-typedef boost::graph_traits<graph_q_t>::vertex_descriptor VertexQ;
-typedef boost::graph_traits<graph_q_t>::edge_descriptor EdgeQ;
+typedef adjacency_list <listS, vecS, undirectedS, vertex_q, edge_q, no_property > graph_q_t;
+typedef graph_traits<graph_q_t>::vertex_descriptor VertexQ;
+typedef graph_traits<graph_q_t>::edge_descriptor EdgeQ;
 
 // Global Variables
 string InputFileName, OutputFileName;
 float MulticastFraction = 0.0;
 unsigned NumVertices = 0;
 set<unsigned> MulticastVertices;
+
 
 int main(int argc, char* argv[])
 {
@@ -86,14 +88,14 @@ int main(int argc, char* argv[])
     assert(OutputFile.is_open() && InputFile.is_open() && "Error opening files");
 
     graph_t Network(0);
-    boost::dynamic_properties dp;
-    auto name = boost::get(&vertex_p::name, Network);
+    dynamic_properties dp;
+    auto name = get(&vertex_p::name, Network);
     dp.property("node_id", name);
-    auto fillcolor = boost::get(&vertex_p::fillcolor, Network);
+    auto fillcolor = get(&vertex_p::fillcolor, Network);
     dp.property("fillcolor", fillcolor);
-    auto weight = boost::get(&edge_p::weight, Network);
+    auto weight = get(&edge_p::weight, Network);
     dp.property("weight", weight); 
-    auto penwidth = boost::get(&edge_p::penwidth, Network);
+    auto penwidth = get(&edge_p::penwidth, Network);
     dp.property("penwidth", penwidth); 
 
     try
@@ -121,7 +123,7 @@ int main(int argc, char* argv[])
 
     // TODO: Add timing code
     
-    NumVertices = boost::num_vertices(Network);
+    NumVertices = num_vertices(Network);
     unsigned NumMulticast = (unsigned)MulticastFraction * NumVertices;
 
     // Debug
@@ -146,7 +148,7 @@ int main(int argc, char* argv[])
     MulticastVertices.insert(0);
     MulticastVertices.insert(1);
     MulticastVertices.insert(2);
-    MulticastVertices.insert(8);
+    MulticastVertices.insert(3);
 
     // TODO: Fix possible issue for Vertex descriptor mismatch
     // Note : Vertex Name is an unsigned int which is the same type as 
@@ -155,15 +157,15 @@ int main(int argc, char* argv[])
 
     // Debug
     ostream dbg(std::cout.rdbuf());
-    //write_graphviz_dp(dbg, Network, dp);
+    write_graphviz_dp(dbg, Network, dp);
 
     graph_q_t G1(0);
     map<unsigned, VertexQ> RMap;
 
     for(auto &V : MulticastVertices)
     {
-        VertexQ Vx = boost::add_vertex(G1); 
-        cout << Vx << " " << V << "\n";
+        VertexQ Vx = add_vertex(G1); 
+        //cout << Vx << " " << V << "\n";
         G1[Vx].label = V;
         G1[Vx].name = Vx;
         RMap.insert(make_pair(V,Vx));
@@ -171,14 +173,15 @@ int main(int argc, char* argv[])
 
 
     // Debug
-    boost::dynamic_properties dp_q;
-    auto name_q = boost::get(&vertex_q::name, G1);
+    dynamic_properties dp_q;
+    auto name_q = get(&vertex_q::name, G1);
     dp_q.property("node_id", name_q);
-    auto label_q = boost::get(&vertex_q::label, G1);
+    auto label_q = get(&vertex_q::label, G1);
     dp_q.property("label", label_q);
-    write_graphviz_dp(dbg, G1, dp_q);
+    //write_graphviz_dp(dbg, G1, dp_q);
     
     map<Vertex, vector<Vertex> > AllPreds;
+    map<Vertex, vector<unsigned> > AllDistances;
     // Step 1 -- Construct undirected distance graph G1, G and S.
     for(auto V = MulticastVertices.begin(), E = MulticastVertices.end(); V != E; V++)
     {
@@ -187,31 +190,32 @@ int main(int argc, char* argv[])
         vector<unsigned> D(NumVertices);
 
         AllPreds.insert(pair<Vertex,vector<Vertex> >(Vx, vector<Vertex>(NumVertices)));
+        AllDistances.insert(pair<Vertex,vector<unsigned> >(Vx, vector<unsigned>(NumVertices)));
                                 
         dijkstra_shortest_paths(Network, Vx,
-                                weight_map(boost::get(&edge_p::weight,Network))
-                                .distance_map(make_iterator_property_map(D.begin(), get(vertex_index, Network)))
+                                weight_map(get(&edge_p::weight,Network))
+                                .distance_map(make_iterator_property_map(AllDistances[Vx].begin(), get(vertex_index, Network)))
                                 .predecessor_map(make_iterator_property_map(AllPreds[Vx].begin(), get(vertex_index, Network))));
 
         for(auto W = std::next(V); W != E; W++)
         {
             Vertex Wx = *W; 
             //// Debug
-            cout << "Adding Edge (Vx, Wx, D[Wx]): " << Vx << "," << Wx << "," << D[Wx] << endl;
+            //cout << "Adding Edge (Vx, Wx, D[Wx]): " << Vx << "," << Wx << "," << D[Wx] << endl;
             EdgeQ e; bool found;
-            boost::tie(e,found) = boost::add_edge(RMap[*V], RMap[*W], G1);
-            G1[e].weight = D[Wx];
+            tie(e,found) = add_edge(RMap[*V], RMap[*W], G1);
+            G1[e].weight = AllDistances[Vx][Wx];
         }
     }
 
     // Debug
-    write_graphviz_dp(dbg, G1, dp_q);
+    //write_graphviz_dp(dbg, G1, dp_q);
 
     // Step 2a -- Construct Minimum Spanning Tree from G1
     
     vector<EdgeQ> st;
     kruskal_minimum_spanning_tree(G1, back_inserter(st), 
-                                  weight_map(boost::get(&edge_q::weight, G1)));
+                                  weight_map(get(&edge_q::weight, G1)));
 
     // Step 2b -- Trim G1 based on MST
     
@@ -237,9 +241,76 @@ int main(int argc, char* argv[])
             eRemove.insert(e);
     }
 
-    for(auto Ex : eRemove) boost::remove_edge(Ex, G1);
-    for(auto Vx : vRemove) boost::remove_vertex(Vx, G1);
+    for(auto Ex : eRemove) remove_edge(Ex, G1);
+    for(auto Vx : vRemove) remove_vertex(Vx, G1);
 
+    write_graphviz_dp(dbg, G1, dp_q);
+
+    // Step 3 -- Add the shortest paths back to G1 (expand each edge)
+   
+    map<unsigned, VertexQ> labelVertexQmap;
+    set<unsigned> addedLabels;
+    for(auto Vx: vKeep) 
+    {
+        addedLabels.insert(G1[Vx].label);
+        labelVertexQmap.insert(make_pair(G1[Vx].label, Vx));
+    }
+
+    for(auto &Ex : eKeep)
+    {
+        cout << "Source: " << G1[source(Ex,G1)].label << "\n";
+        auto predecessor_map = AllPreds[G1[source(Ex,G1)].label];
+
+        // Traverse from sink to source
+        
+        Vertex v = G1[target(Ex, G1)].label;
+        cout << "Target: " << G1[target(Ex,G1)].label << "\n";
+        cout << "Going from: " << v << " -> " << predecessor_map[v] << "\n";
+        bool edge_flag = true;
+        for(Vertex u = predecessor_map[v];          // Start by setting 'u' to the destintaion node's predecessor
+                   u != v;                          // Keep tracking the path until we get to the source
+                   v = u, u = predecessor_map[v])   // Set the current vertex to the current predecessor, and the predecessor to one level up 
+        {
+            // Get the edge from the Network Graph
+            Edge e; bool f = false;
+            tie(e, f) = edge(u, v, Network);
+            assert(f && "Could not find edge in original graph!");           
+            cout << "Found " << source(e, Network) << " - " << target(e, Network) << "\n";
+            double wt = Network[e].weight;
+
+            if(addedLabels.count(u) == 0)
+            {
+                // Add a vertex and thus also add an edge
+                VertexQ Vx = add_vertex(G1);
+                G1[Vx].label = u;
+                labelVertexQmap.insert(make_pair(u,Vx));
+
+                VertexQ Wx = labelVertexQmap[v];
+                EdgeQ eq; bool fq = false;
+                tie(eq, fq) = add_edge(Vx, Wx, G1);
+                G1[eq].weight = wt;
+            }
+            else
+            {
+                // Label (and thus vertex) already added 
+                // check if edge exists, if not add edge
+                VertexQ Vx = labelVertexQmap[u], Wx = labelVertexQmap[v];
+                EdgeQ eq; bool fq = false;
+                tie(eq, fq) = edge(Vx, Wx, G1);
+                if(!fq)
+                {
+                    // Edge not found
+                    tie(eq, fq) = add_edge(Vx, Wx, G1);
+                    G1[eq].weight = wt;
+                }
+                else
+                    edge_flag = false;
+            }
+        }
+        if(edge_flag)
+            remove_edge(Ex, G1); // ??
+    }
+    
     write_graphviz_dp(dbg, G1, dp_q);
 
     //try
