27005bfc7e0f9516e0d08b8983162439464a9f26
Snehasish Kumar <ska124@sfu.ca>
2015-03-27 12:44:34 -0700

    DEBUG define works

--- Makefile
+++ Makefile
@@ -5,10 +5,13 @@ LIB_PATHS=-Wl,-rpath=$(HOME)/Apps/boost_1_55_0/lib -L$(HOME)/Apps/boost_1_55_0/l
 all: KMB.cpp 
 	$(CXX) -O3 -std=c++11 $(INCLUDE_PATHS) $(LIB_PATHS) KMB.cpp -o KMB -lboost_graph
 
-test:
-	./KMB 0.2 test.dot out.dot
+debug: KMB.cpp 
+	$(CXX) -DDEBUG -ggdb3 -std=c++11 $(INCLUDE_PATHS) $(LIB_PATHS) KMB.cpp -o KMB-dbg -lboost_graph
+
+test: debug
+	./KMB-dbg 0.1 steiner.dot debug.dot
 
 clean:
-	rm -f KMB out.dot 
+	@rm -f KMB KMB-dbg debug.dot 
 
 .PHONY: clean test
--- brite2dot.sh
+++ brite2dot.sh
@@ -4,5 +4,5 @@ britefile=$1
 dotfile=`echo "${britefile}" | sed -n 's/brite/dot/p'`
 echo "graph G{" > ${dotfile}
 sed -n '/Nodes:/,/^$/p' ${britefile} | tail -n +2 | sed '/^$/d' | awk '{printf "%d;\n",$1}' >> ${dotfile}
-sed -n '/Edges:/,/^$/p' ${britefile} | tail -n +2 | sed '/^$/d' | awk '{printf "%d--%d; [weight=%f]\n",$2,$3,$4}' >> ${dotfile}
+sed -n '/Edges:/,/^$/p' ${britefile} | tail -n +2 | sed '/^$/d' | awk '{printf "%d--%d; [weight=%.2f]\n",$2,$3,$4}' >> ${dotfile}
 echo "}" >> ${dotfile}
