e9e408161ef1bafab5a3c5383545f4ee30b755b5
Snehasish Kumar <ska124@sfu.ca>
2015-03-28 22:51:10 -0700

    fixed bug in brite2dot.sh, adding sample graphs of size ~6000 nodes

--- brite2dot.sh
+++ brite2dot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 britefile=$1
-dotfile=`echo "${britefile}.dot" 
+dotfile=`echo "${britefile}.dot"`
 echo "graph G{" > ${dotfile}
 sed -n '/Nodes:/,/^$/p' ${britefile} | tail -n +2 | sed '/^$/d' | awk '{printf "%d;\n",$1}' >> ${dotfile}
 sed -n '/Edges:/,/^$/p' ${britefile} | tail -n +2 | sed '/^$/d' | awk '{printf "%d--%d [weight=%.2f];\n",$2,$3,$4}' >> ${dotfile}
